caeff09e0b354001464a73ce2d9952f430dce51c
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2020-12-31 13:30:26 +0100

    neuron zrobiony

--- lab11/neuron.py
+++ lab11/neuron.py
@@ -1,35 +1,85 @@
 import random
-from numpy import tanh, exp
+from numpy import tanh, exp, heaviside
+import csv
+
+
+def read_input_data(filename):
+    """
+    Reads input data (train / test) from a CSV file.
+    Input:
+        filename - CSV file name (string)
+    CSV file format:
+        input1, input2, ..., output
+                        ...
+                        ...
+    Returns:
+        Nin - number of inputs of the perceptron (int)
+        X - input training data (list)
+        Y - output (expected) training data (list)
+    """
+
+    X = []
+    Y = []
+
+    with open(filename, 'r') as file:
+        dataset = csv.reader(file, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
+
+        line1 = next(dataset)
+        Nin = len(line1) - 1
+        X.append(line1[0:Nin])
+        Y.append(line1[Nin])
+
+        for line in dataset:
+            X.append(line[0:Nin])
+            Y.append(line[Nin])
+    return Nin, X, Y
 
 
 class neuron():
-
-    sigmoid = lambda x: 1/(1+exp(-x))
+    sigmoid = lambda x: 1 / (1 + exp(-x))
+    sigmoid_prim = lambda x, s=sigmoid: s(x) * (1 - s(x))
     relu = lambda x: max(0, x)
-    activations = {"tanh": tanh, "sigm": sigmoid, "relu": relu}
+    relu_prim = lambda x: heaviside(x, 1)
+    tanh_prim = lambda x, a=tanh: 1 - a(x) ** 2
 
-    def __init__(self, input_number, activation="tanh", bias=0):
+    activations = {"tanh": (tanh, tanh_prim), "sigm": (sigmoid, sigmoid_prim), "relu": (relu, relu_prim)}
+
+    def __init__(self, input_number, activation="tanh", bias=0.0):
         self.input_number = input_number
         self.weights = [random.random() for i in range(input_number)]
         self.bias = bias
         try:
-            self.activation = self.activations[activation]
+            self.activation = self.activations[activation][0]
+            self.activation_derivative = self.activations[activation][1]
         except KeyError:
             print("Nie zaimplementowano takiej funkcji aktywacji: " + activation)
             exit(1)
 
-    # TODO: jakieś trenowanko czy coś
+    def train(self, epochs, X, Y, eta=0.1):
+        for epoch in range(epochs):
+            print("Epoch no. {}".format(epoch))
+
+            weight_sum = sum(self.weights)
+
+            for i in range(len(X)):
+                Y_out = tanh(weight_sum)
+
+                for j in range(self.input_number):
+                    self.weights[j] += eta * self.activation_derivative(weight_sum) * (Y[i] - Y_out) * X[i][j]
 
     def output(self, input):
         sum = 0
         for index, value in enumerate(input):
             sum += value * self.weights[index]
 
-        # TODO: to na pewno było tak? ;p
         return self.activation(sum + self.bias)
 
 
 if __name__ == '__main__':
-    neuron = neuron(5)
-    print(neuron.activation(0))
-    print(neuron.weights)
+    Nin, X, Y = read_input_data("train_data.csv")
+    n = neuron(Nin)
+    print(n.weights)
+    print(n.output([0.2, 0.1]))
+    n.train(10000, X, Y, eta=0.4)
+    print(n.weights)
+    print(n.output([0.2, 0.1]))
